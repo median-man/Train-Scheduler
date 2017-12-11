365bf5239622bdcc15ae2487a3684f1397d9a787
John Desrosiers <johndrunner@yahoo.com>
2017-12-11 06:06:59 -0800

    refactor add train

--- assets/javascript/trainScheduler.js
+++ assets/javascript/trainScheduler.js
@@ -11,75 +11,52 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// render table whenever data changes. on with "child_added" runs callback
-// for each child of node when on is first called and any time a child is
-// added. each child is an object containing data for one train.
-database.ref().on('child_added', (trainSnap, prevTrainKey) => {
-  // notify train added
-  trainAddedToSchedule(trainSnap.toJSON());
-});
+// display new trains when added to database
+database.ref().on('child_added', trainSnap => trainAddedToSchedule(trainSnap.toJSON()));
 
-
-// Adds train to database
+// Format user input and add to database
 function addNewTrain(train) {
-  // convert first train time into a mommentjs object
-  train.firstTime = moment(train.firstTime, 'H:mm').unix();
-
-  // convert frequency to an integer
-  train.freq = parseInt(train.freq);
-
-  // add train to the database
   database.ref().push(train);
 }
 
 // Appends train to table
 function trainAddedToSchedule(train) {
-  const mNow = moment();
-
-  // moment for first train time
+  // calculate minutes to next train and next train time
   const mFirstTrain = moment.unix(train.firstTime);
-
-  // time since first train
   const minElapsed = moment().diff(mFirstTrain, 'minutes');
-
-  // time until next train
   const minToNext = train.freq - minElapsed % train.freq;
-
-  // time of next train
   const timeNext = moment().add(minToNext, 'm').format('h:mm A');
 
-  // create a new row
+  // add row to table
   const row = $('<tr>');
-
-  // add data to the row
   $('<td>').text(train.name).appendTo(row);
   $('<td>').text(train.dest).appendTo(row);
   $('<td>').text(train.freq).appendTo(row);
   $('<td>').text(timeNext).appendTo(row);
   $('<td>').text(minToNext).appendTo(row);
-
-  // append the row
   $('tbody').append(row);
+
+  // TODO update times every minute
 }
 
 $(document).ready(() => {
   // get form data and save it to the database whenever user clicks submit
   $('#btnSubNewTrain').on('click', (event) => {
-    // prevent form from attempting to submit form and reloading the
-    // page
     event.preventDefault();
 
-    // get data from the form
-    const train = {};
-    train.name = $('#txtTrainName').val().trim();
-    train.dest = $('#txtDestination').val().trim();
-    train.firstTime = $('#txtFirstTrain').val().trim();
-    train.freq = $('#txtFrequency').val().trim();
+    // convert time to unix timestamp
+    let firstTime = $('#txtFirstTrain').val().trim();
+    firstTime = moment(firstTime, 'H:mm').unix();
+    
+    // get user input and pass it to handler 
+    addNewTrain({
+      name: $('#txtTrainName').val().trim(),
+      dest: $('#txtDestination').val().trim(),
+      firstTime,
+      freq: parseInt($('#txtFrequency').val().trim()),
+    });
 
     // reset the form
     $('txtTrainName, #txtDestination, #txtFirstTrain, #txtFrequency').val('');
-
-    // handle form data
-    addNewTrain(train);
   });
 });
