b1eaa4fa7471307807215fdba35e17a6319e7bc4
John Desrosiers <johndrunner@yahoo.com>
2017-12-11 06:57:38 -0800

    train times are continuously updated at regular intervals

--- assets/javascript/trainScheduler.js
+++ assets/javascript/trainScheduler.js
@@ -9,14 +9,36 @@ const config = {
 };
 firebase.initializeApp(config);
 
+// globals
 const database = firebase.database();
-
-// display new trains when added to database
-database.ref().on('child_added', trainSnap => trainAddedToSchedule(trainSnap.toJSON()));
 
 // Format user input and add to database
 function addNewTrain(train) {
   database.ref().push(train);
+}
+
+// Function to calculate and format minutes to next train and next train time
+function getTimes(tsFirst, frequency) {
+  // calculate minutes to next train and next train time
+  const mFirstTrain = moment.unix(tsFirst);
+  const minElapsed = moment().diff(mFirstTrain, 'minutes');
+  const minToNext = frequency - minElapsed % frequency;
+  const timeNext = moment().add(minToNext, 'm').format('h:mm A');
+  return { minToNext, timeNext };
+}
+
+// Function to update train times for every row from data stored in element
+function updateTimes() {
+  $('.train').each(function updateRow() {
+    const $row = $(this);
+    // calculate times from data
+    const trainData = $row.data();
+    const times = getTimes(trainData.tsFirstTrain, trainData.frequency);
+
+    // update text
+    $row.children().eq(3).text(times.timeNext);
+    $row.children().eq(4).text(times.minToNext);
+  });
 }
 
 // Appends train to table
@@ -28,18 +50,24 @@ function trainAddedToSchedule(train) {
   const timeNext = moment().add(minToNext, 'm').format('h:mm A');
 
   // add row to table
-  const row = $('<tr>');
+  const row = $('<tr>')
+    .data({ tsFirstTrain: train.firstTime, frequency: train.freq })
+    .addClass('train');
   $('<td>').text(train.name).appendTo(row);
   $('<td>').text(train.dest).appendTo(row);
   $('<td>').text(train.freq).appendTo(row);
   $('<td>').text(timeNext).appendTo(row);
   $('<td>').text(minToNext).appendTo(row);
   $('tbody').append(row);
-
-  // TODO update times every minute
 }
 
 $(document).ready(() => {
+  // display new trains when added to database
+  database.ref().on('child_added', trainSnap => trainAddedToSchedule(trainSnap.val()));
+
+  // update train times every 20 seconds
+  setInterval(updateTimes, 20000);
+
   // get form data and save it to the database whenever user clicks submit
   $('#btnSubNewTrain').on('click', (event) => {
     event.preventDefault();
